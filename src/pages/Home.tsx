67e891cca8ed46edcdacf255625ee9290acc6bbb
pandamy619 <vladislavtagaev@gmail.com>
2025-03-25 15:36:44 +0300

    feat: add tailwind css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <h1>🏠 Главная страница</h1>;
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-500">
+          Hello, TailwindCSS!
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default Home;
